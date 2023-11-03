af2c8fb466b450347f93d175ec6d2889818a872c
Yduuduu <hou_o@naver.com>
2023-11-03 15:37:59 +0900

    ✨ ui구성 및 getStaticProps

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetails from "./../../components/meetups/MeetupDetails";
+
+function MeetupDetailsPage() {
+  return (
+    <MeetupDetails
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Cross_Harbour_Tunnel_%281%29.JPG/1280px-Cross_Harbour_Tunnel_%281%29.JPG"
+      title="A First Meetup"
+      address="Some address 5, 1234"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetailsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,34 @@
-export default function HomePage() {
-  return <div>The Home Page</div>;
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Cross_Harbour_Tunnel_%281%29.JPG/1280px-Cross_Harbour_Tunnel_%281%29.JPG",
+    address: "Some address 5, 1234",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Cross_Harbour_Tunnel_%281%29.JPG/1280px-Cross_Harbour_Tunnel_%281%29.JPG",
+    address: "Some address 51, 4323",
+    description: "This is a Second meetup",
+  },
+];
+
+export default function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10,
+  };
 }
